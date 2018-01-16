fd0412be420dee9dd32db4c5002eaa5ea58f6c57
Ron Sala <ron.sala@gmail.com>
2018-01-16 04:06:43 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,11 +1,13 @@
 class School
 
+  attr_accessor :roster
+
   def initialize(name)
     @name = name
   end
 
-  def roster
-    @roster.hash ^ @roster.hash
-  end
+  # def roster
+  #   @roster.hash ^ @roster.hash
+  # end
 
 end
